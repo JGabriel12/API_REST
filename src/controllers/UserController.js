1c5e0234e50aaa338e39b633a4feeead2f54581f
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2023-02-28 17:36:25 -0300

    User Controller issue fix

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,15 +3,10 @@ import User from '../models/User';
 class HomeController {
   async store(req, res) {
     try {
-      const novoUser = await User.create({
-        name: 'Gabriel',
-        last_name: 'Marinho',
-        email: 'gabriel.marinho@hotmail.com',
-        password: '123456',
-      });
-      res.json(novoUser);
+      await User.create(req.body);
+      res.status(200).json('User Created Successful!');
     } catch (error) {
-      res.send(error);
+      res.status(400).json(error);
     }
   }
 }
