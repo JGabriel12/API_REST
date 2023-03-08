f63ac5562b61eb850399c4499caf0d99290caf6c
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2023-03-08 16:00:09 -0300

    multer controller and error catch

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -2,6 +2,15 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png'];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new multer.MulterError('Invalid type file!'));
+    }
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import imageController from '../controllers/imageController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('image'), imageController.store);
+router.post('/', imageController.store);
 
 export default router;
